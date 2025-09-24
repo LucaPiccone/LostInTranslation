165d4475ff2cffdd300c4b0d54bade9aa509537b
Luca Piccone <lpiccone19@gmail.com>
2025-09-24 17:56:57 -0400

    Updated the GUI

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -2,6 +2,8 @@ package translation;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 // TODO Task D: Update the GUI for the program to align with UI shown in the README example.
@@ -14,16 +16,38 @@ public class GUI {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JPanel countryPanel = new JPanel();
-            JTextField countryField = new JTextField(10);
-            countryField.setText("can");
-            countryField.setEditable(false); // we only support the "can" country code for now
+            // Get all country codes
+            JSONTranslator jsonTranslator = new JSONTranslator();
+            List<String> countryCodes = jsonTranslator.getCountryCodes();
+            // convert country codes to country name
+            CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
+            // create a list of all countries.
+            List<String> countries = new ArrayList<>();
+            for (String countryCode : countryCodes) {
+                countries.add(countryCodeConverter.fromCountryCode(countryCode));
+            }
+            // create a drop down menu and add all countries to the dropdown
+            JComboBox comboBox = new JComboBox();
+            for (String country : countries) {
+                comboBox.addItem(country);
+            }
             countryPanel.add(new JLabel("Country:"));
-            countryPanel.add(countryField);
+            countryPanel.add(comboBox);
 
+            // create a JList
             JPanel languagePanel = new JPanel();
-            JTextField languageField = new JTextField(10);
+            // get all language codes
+            List<String> languageCodes = jsonTranslator.getLanguageCodes();
+            // convert language codes to language names
+            LanguageCodeConverter languageCodeConverter = new LanguageCodeConverter();
+            // create a list of all languages
+            List<String> languages = new ArrayList<>();
+            for (String languageCode : languageCodes) {
+                languages.add(languageCodeConverter.fromLanguageCode(languageCode));
+            }
+            JList<String> jlist = new JList<>(languages.toArray(new String[0]));
             languagePanel.add(new JLabel("Language:"));
-            languagePanel.add(languageField);
+            languagePanel.add(jlist);
 
             JPanel buttonPanel = new JPanel();
             JButton submit = new JButton("Submit");
@@ -39,19 +63,29 @@ public class GUI {
             submit.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    String language = languageField.getText();
-                    String country = countryField.getText();
+                    String country = (String) comboBox.getSelectedItem();
+                    String language = jlist.getSelectedValue();
+                    System.out.println(country);
+                    System.out.println(language);
 
-                    // for now, just using our simple translator, but
-                    // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
+                    // country is the 3 digit country code.
+                    CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
+                    String countryCode = countryCodeConverter.fromCountry(country);
 
-                    String result = translator.translate(country, language);
+                    LanguageCodeConverter languageCodeConverter = new LanguageCodeConverter();
+                    String languageCode = languageCodeConverter.fromLanguage(language);
+
+                    System.out.println(countryCode);
+                    System.out.println(languageCode);
+
+                    JSONTranslator jsonTranslator = new JSONTranslator();
+
+                    String result = jsonTranslator.translate(countryCode, languageCode);
+
                     if (result == null) {
                         result = "no translation found!";
                     }
                     resultLabel.setText(result);
-
                 }
 
             });
